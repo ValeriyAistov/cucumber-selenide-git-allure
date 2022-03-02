8b373fbab047e4c95b089d4ceb79c9149a7010e6
aistov <aistov@alkor.co.ru>
2022-03-02 09:10:09 +0300

    change pass

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -2,5 +2,5 @@ package config;
 
 public class UserConfig {
     public static final String LOGIN = "v-aistov2017@yandex.ru";
-    public static final String PASS = "1289191";
+    public static final String PASS = "1289191asa";
 }
