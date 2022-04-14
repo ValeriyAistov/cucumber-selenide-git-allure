f2476134a0a45817078621da2f53a0e7fc4eafc4
ValeriyAistov <loki19960807@mail.ru>
2022-04-14 09:08:16 +0300

    add

--- src/test/java/Steps/Hooks.java
+++ src/test/java/Steps/Hooks.java
@@ -11,9 +11,4 @@ public class Hooks {
         System.setProperty("webdriver.chrome.driver","D:\\Тестировка\\chromedriver.exe");
         open("https://yandex.ru/");
     }
-
-    public void closeUrl(){
-        System.setProperty("webdriver.chrome.driver","D:\\Тестировка\\chromedriver.exe");
-        open("https://yandex.ru/");
-    }
 }
